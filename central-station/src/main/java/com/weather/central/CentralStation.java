43cd2bd914f6a4db4305c58efad4a3d5f00dab93
naduto <nadagamal5078@gmail.com>
2026-05-18 16:33:48 +0300

    project structure

---
+++ central-station/src/main/java/com/weather/central/CentralStation.java
@@ -0,0 +1,9 @@
+package com.weather.central;
+
+public class CentralStation {
+
+    public static void main(String[] args) {
+        System.out.println("Central Station running!");
+    }
+
+}
---
+++ weather-station/src/main/java/com/weather/station/WeatherStation.java
@@ -0,0 +1,9 @@
+package com.weather.station;
+
+public class WeatherStation {
+
+    public static void main(String[] args) {
+        System.out.println("Weather Station running!");
+    }
+
+}
